18de41fdd289d5d8ada76ed37125869d00af0832
Frank <40611willy@gmail.com>
2022-12-13 06:12:02 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    request(
+        'https://apps.nutc.edu.tw/getParking/showParkingData.php', ((error, response, body) => {
+            res.json({ msg: body, error: error, response: response });
+        }));
+});
+app.listen(process.env.PORT || 80);
