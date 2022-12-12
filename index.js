353f9fd4d1c4f33c33740872f3691a76d2beb475
Frank <40611willy@gmail.com>
2022-12-13 06:50:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const request = require("request");
 const app = express();
 
 app.get('/', (req, res) => {
-    request('https://myip.com.tw/', ((error, response, body) => {
-        res.json({ msg: body, error: error, response: response });
-    }));
+    res.send("<h1>It's working 🤗</h1>")
 });
 
 const port = process.env.PORT || 8080
